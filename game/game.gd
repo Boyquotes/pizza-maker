22efe6f57ca6686fad846f8fbfa1111c052fa328
Dave <48969490+sugarvoid@users.noreply.github.com>
2023-05-13 16:55:22 -0500

    Add topping functions and day of week

--- game/game.gd
+++ game/game.gd
@@ -1,51 +1,90 @@
 extends Node2D
 
 
+var day_of_week: Array = [
+	"Mon", "Tues", "Wed", "Thurs", "Fri"
+]
+
+var cash: float
 var is_shift_held: bool 
+var break_time: float = 2.0
+var time_per_round: float = 6.0
+
+var daily_income: float
+var daily_tips: float
+
+
 onready var pizza: Pizza = get_node("Pizza")
+onready var animation_player: AnimationPlayer = $AnimationPlayer
+onready var trm_next_order: Timer = get_node("TrmNextOrder")
+onready var trm_order_time: Timer = get_node("TrmOrderTime")
+
 
 
 func _ready() -> void:
-	pass
+	self.animation_player.connect("animation_finished", self, "_on_animation_done")
+	self.trm_next_order.connect("timeout", self, "_start_new_round")
+	self.trm_order_time.connect("timeout", self, "_end_round")
 
 func _process(delta: float) -> void:
 	self.is_shift_held = Input.is_action_pressed("left_shift")
 	$HUD.update_labels(self.is_shift_held)
 	
-	
+
 func _unhandled_key_input(event: InputEventKey) -> void:
 	if event.is_action_pressed("ui_accept"):
 		# $Pizza.pulse()
 		if self.is_shift_held:
 			self.pizza.cook()
 		else:
-			$AnimationPlayer.play("slide_pizza_out")
+			self.animation_player.play("slide_pizza_out")
 			# start timer to get new order and send in blank pizza
 		
 	if event.is_action_pressed("up_topping"):
-		$Pizza.pulse()
+		self.pizza.pulse()
 		if self.is_shift_held:
 			self.pizza.add_topping(Global.toppings.alt_up)
 		else:
 			self.pizza.add_topping(Global.toppings.up)
 	elif event.is_action_pressed("down_topping"):
-		$Pizza.pulse()
+		self.pizza.pulse()
 		if self.is_shift_held:
 			self.pizza.add_topping(Global.toppings.alt_down)
 		else:
 			self.pizza.add_topping(Global.toppings.down)
 	elif event.is_action_pressed("left_topping"):
-		$Pizza.pulse()
+		self.pizza.pulse()
 		if self.is_shift_held:
 			self.pizza.add_topping(Global.toppings.alt_left)
 		else:
 			self.pizza.add_topping(Global.toppings.left)
 	elif event.is_action_pressed("right_topping"):
-		$Pizza.pulse()
+		self.pizza.pulse()
 		if self.is_shift_held:
 			self.pizza.add_topping(Global.toppings.alt_right)
 		else:
 			self.pizza.add_topping(Global.toppings.right)
 		
 
+func _start_new_day() -> void:
+	pass
+	# Advnace day
+	# Update day of week on HUD
+
+func _start_new_round() -> void:
+	self.animation_player.play("slide_pizza_in")
+	
+
+func _end_round() -> void:
+	print("time ran out, customer left")
+
+func _on_animation_done(anim_name: String):
+	match anim_name:
+		"slide_pizza_in":
+			print("new pizza on table")
+		"slide_pizza_out":
+			print("pizza has been served")
+			self.pizza.reset()
+			self.trm_next_order.start(self.break_time)
+			# Reset pizza
 
--- game/pizza/pizza.gd
+++ game/pizza/pizza.gd
@@ -28,11 +28,12 @@ var seconds = 0.02
 func _ready() -> void:
 	$Tween.connect("tween_completed", self, "_tween_done")
 
-func reset_pizza() -> void:
+func reset() -> void:
 	self.modulate = "ffffff"
 	$SteamSprites.visible = false
 	self.toppings = []
 	self.is_cooked = false
+	self.position = Vector2(-98,138)
 	
 
 
@@ -53,12 +54,18 @@ func _get_steam_pos() -> Vector2:
 	return Vector2(ranX,ranY)
 
 func cook() -> void:
-	_add_steam()
-	self.is_cooked = true
+	if !self.is_cooked:
+		_add_steam()
+		self.is_cooked = true
+	else:
+		self._burn()
 
 func _add_steam() -> void:
 	self.modulate = "e5c7c7"
 	$SteamSprites.visible = true
+
+func _burn() -> void:
+	self.modulate = "604a4a"
 
 func _scale_down() -> void:
 	$Tween.interpolate_property(self, "scale", self.scale, scaleDown, seconds, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
@@ -75,8 +82,28 @@ func _tween_done(object: Object, node_path: NodePath):
 
 func add_topping(topping: String) -> void:
 	if !self.toppings.has(topping):
+		match(topping):
+			Global.toppings.up: # ex cheese
+				pass
+			Global.toppings.down: # chicken
+				pass
+			Global.toppings.left: # pepperoni
+				pass
+			Global.toppings.right: # olives
+				pass
+			Global.toppings.alt_up: # pineapple
+				pass
+			Global.toppings.alt_down: # mushroom
+				pass
+			Global.toppings.alt_left: # g peppers
+				pass
+			Global.toppings.alt_right: # onion
+				pass
+
 		print_debug(str("adding ", topping))
 		self.toppings.append(topping)
+		# Set sprite to random rotation
+		# Make sprite visible
+		
 	else:
 		print(str("Pizza already has ", topping.capitalize()))
-	
